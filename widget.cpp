442dc88f7e4f2a8300f8ea4ad5ebe73964cdcff0
meokisama <hoangdinhsang123@gmail.com>
2021-03-20 13:51:35 +0700

    add more icons & config something

--- widget.cpp
+++ widget.cpp
@@ -17,3 +17,33 @@ void Widget::on_closeButton_clicked()
 {
     close();
 }
+
+void Widget::on_btnHome_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(0);
+}
+
+void Widget::on_btnLiveChat_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(1);
+}
+
+void Widget::on_btnCalendar_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(2);
+}
+
+void Widget::on_btnVideo_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(3);
+}
+
+void Widget::on_btInfo_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(4);
+}
+
+void Widget::on_btnUser_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(5);
+}
--- widget.h
+++ widget.h
@@ -18,6 +18,18 @@ public:
 private slots:
     void on_closeButton_clicked();
 
+    void on_btnHome_clicked();
+
+    void on_btnLiveChat_clicked();
+
+    void on_btnCalendar_clicked();
+
+    void on_btnVideo_clicked();
+
+    void on_btInfo_clicked();
+
+    void on_btnUser_clicked();
+
 private:
     Ui::Widget *ui;
 };
